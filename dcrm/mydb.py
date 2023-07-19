7cc216874cf6dd562907da6da3b5bdbf447d620c
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-19 10:15:37 +0400

    succesfully run -python  manage.py runserver

--- dcrm/mydb.py
+++ dcrm/mydb.py
@@ -8,12 +8,11 @@ import mysql.connector
 dataBase = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    password = 'mAs27121995@@',
-    database = '127.0.0.1'
+    passwd = 'mAs27121995@@',
 )
 #prepare a cusor object
 cursorObject = dataBase.cursor()
 
 #create a database
-cursorObject.execute("CREATE DATABASE mascollection")
-print("Database is All Done")
+cursorObject.execute("CREATE DATABASE MUHAMMED")
+print("All Done")
