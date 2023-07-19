715408270e9f696c2f7b7fb10e1575a3829b7b07
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-19 08:17:34 +0400

    import mysql database

---
+++ dcrm/mydb.py
@@ -0,0 +1,15 @@
+#install mySQL
+#pip install mysql
+#pip install mysql connector
+#pip install mysql connector_python
+
+import mysql.connector
+
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'mAs27121995@@'
+    
+
+)
